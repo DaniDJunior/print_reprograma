b23f9166f543aac8d190b871040b35dbec916112
MagoMatematico <dani@travatech.com.br>
2024-07-05 18:17:51 -0300

    nova versao

--- print_reprograma/__init__.py
+++ print_reprograma/__init__.py
@@ -1,11 +1,1 @@
-"""
-Print Reprograma.
-
-Essa é um componente de print com esteroides para deixar nosso terminal mais bunitinho.
-"""
-
-__version__ = "0.1.0"
-__author__ = 'Daniele Junior'
-__credits__ = '{Reprograma}'
-
 from .main import imprimir
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='print_reprograma',
-    version='0.1.0',    
+    version='0.1.1',    
     description='A example Python package',
     url='https://github.com/DaniDJunior/print_reprograma',
     author='Daniele Junior',
