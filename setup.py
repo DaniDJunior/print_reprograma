a2d96e74d8391fa21af68e3ee9628301f011800d
MagoMatematico <dani@travatech.com.br>
2024-07-08 14:47:35 -0300

    long_description

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     name='print_reprograma',
     version='0.1.2',    
     description='A example Python package',
