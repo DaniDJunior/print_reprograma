c999e785842dbece63ec636003d8530bb4e82517
MagoMatematico <dani@travatech.com.br>
2024-07-08 14:20:06 -0300

    mudança da versão

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='print_reprograma',
-    version='0.1.1',    
+    version='0.1.2',    
     description='A example Python package',
     url='https://github.com/DaniDJunior/print_reprograma',
     author='Daniele Junior',
